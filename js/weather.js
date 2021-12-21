c689e81312a39e037e58100ebf268f121aa496b1
Malak-Alibrahim <80803259+Malak-Alibrahim@users.noreply.github.com>
2021-12-21 22:22:47 +1100

    fix weather issues

--- js/weather.js
+++ js/weather.js
@@ -19,24 +19,24 @@ weatherButton.addEventListener("click", (e) => {
   weatherInput.value = "";
 });
 
-const getWeather = async (city) => {
+const getWeather = async (weatherInput) => {
   try {
     const response = await fetch(
-      `https://api.weatherbit.io/v2.0/current?city=${city}&key=${key}`
+      `https://api.weatherbit.io/v2.0/current?city=${weatherInput}&key=${key}`
     );
     const weatherData = await response.json();
-    console.log(weatherData.data[0].city_name);
-    
-    const {city_name, temp, datetime, wind_spd, rh} = weatherData.data[0];
-    const {description, icon} = weatherData.data[0].weather;
-    
-    city.textContent = city_name; //TODO: city name doesn't change - fix it
-    time.textContent = datetime; //TODO: date doesn't change - fix it
+    //console.log(weatherData.data[0].city_name);
+
+    const { city_name, temp, datetime, wind_spd, rh } = weatherData.data[0];
+    const { description, icon } = weatherData.data[0].weather;
+
+    city.textContent = city_name;
+    time.textContent = datetime;
     weatherIcon.src = `https://www.weatherbit.io/static/img/icons/${icon}.png`;
     tempValue.textContent = Math.floor(temp) + "°C";
     tempDescription.textContent = description;
     windSpeed.textContent = wind_spd + " m/s";
-    humidity.textContent = rh + "%";   
+    humidity.textContent = rh + "%";
   } catch (error) {
     alert("City not found");
   }
@@ -47,24 +47,24 @@ window.addEventListener("load", () => {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {
       lat = position.coords.latitude;
-      lon = position.coords.longitude;   
+      lon = position.coords.longitude;
       const api = `https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=${key}`;
       fetch(api)
         .then((response) => {
           return response.json();
         })
         .then((data) => {
-          console.log(data.data[0]. city_name);
-          const {city_name, temp, datetime, wind_spd, rh} = data.data[0];
-          const {description, icon} = data.data[0].weather;
-          
+          //console.log(data.data[0]. city_name);
+          const { city_name, temp, datetime, wind_spd, rh } = data.data[0];
+          const { description, icon } = data.data[0].weather;
+
           city.textContent = city_name;
           time.textContent = datetime;
           weatherIcon.src = `https://www.weatherbit.io/static/img/icons/${icon}.png`;
           tempValue.textContent = Math.floor(temp) + "°C";
           tempDescription.textContent = description;
           windSpeed.textContent = wind_spd + " m/s";
-          humidity.textContent = rh + "%";         
+          humidity.textContent = rh + "%";
         });
     });
   }
